db3decc3ef19f898b1d850f6bfaa70a274ec1cf6
Sean Henderson <mail@sean.sh>
2018-05-13 19:19:48 -0700

    set env var for desired color scheme per time of day

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -4,3 +4,10 @@ end
 
 # http://www.fileformat.info/search/google.htm?q=unicode+face
 set -gx __fish_prompt_char ""
+
+# set color scheme based on time of day
+if test (date +%H) -gt 8; and test (date +%H) -lt 18
+  set -gx COLOR light
+else
+  set -gx COLOR dark
+end
