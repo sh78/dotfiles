a0913960a8a66461766e05368e6c191c0152e7be
Sean Henderson <mail@sean.sh>
2018-05-14 19:40:41 -0700

    🛀  fish functions

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -1,24 +1,35 @@
+# custom greeting
+function fish_greeting
+  if type -q fortune
+    fortune -a
+  else
+    # enjoy the silence
+  end
+end
+
 # load user confs
 for file in $HOME/.config/omf/*.load
     source $file
 end
-# less files == faster load
+# Monday, May 14 2018 19:24
+## not much slower than one giant file. it's what's inside that counts
+## 3 files, 2015 MBP i3 8GB
 
-# http://www.fileformat.info/search/google.htm?q=unicode+face
+# >
 set -gx __fish_prompt_char ""
 
 # vi mode!
 fish_vi_key_bindings
 
-# set color scheme based on time of day
+# export a color scheme var based on time of day
 if test (date +%H) -gt 8; and test (date +%H) -lt 18
   set -gx COLOR light
 else
   set -gx COLOR dark
 end
 
-# set iterm color scheme (iterm shell integration must be loaded
-if test $TERM_PROGRAM = "iTerm.app"; and test $COLOR = "light"
+# set iterm color scheme (iterm shell integration must be loaded)
+if type -q it2setcolor; and test $COLOR = "light"
   it2setcolor preset "Solarized Light v2"
 else
   it2setcolor preset "Solarized Dark v2"
