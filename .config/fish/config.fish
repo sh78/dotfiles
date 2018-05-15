0012de8ce300eb841c5294196377d1a23f655120
Sean Henderson <mail@sean.sh>
2018-05-14 19:00:24 -0700

    iTerm: add shell integration for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,5 +4,9 @@ set -gx OMF_PATH "$HOME/.local/share/omf"
 # Customize Oh My Fish configuration path.
 #set -gx OMF_CONFIG "$HOME/.config/omf"
 
+# iTerm fish shell integration
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
+
