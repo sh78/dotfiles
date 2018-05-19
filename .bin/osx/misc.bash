b7b5aafaf0bfabaa19049b32344bf172d1b24779
Sean Henderson <mail@sean.sh>
2018-05-18 23:57:10 -0700

    italics in term; rename a dir

--- .bin/osx/misc.bash
+++ .bin/osx/misc.bash
@@ -12,16 +12,20 @@ fi
 # misc
 echo "downloading some miscellaneous payloads to the desktop for manual labor..."
 wget http://ethanschoonover.com/solarized/files/solarized.zip
-wget http://cdn3.brettterpstra.com/downloads/TabLinks.2.0.zip
 
 mkdir ~/Desktop/MANUAL\ LABOR
 mv *.zip ~/Desktop/MANUAL\ LABOR
 
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-mv $HERE/app\ presets ~/Desktop/MANUAL\ LABOR
+mv $HERE/solarized.zip ~/Desktop/MANUAL\ LABOR
 
 # set up iTerm2 shell integration
 curl -L https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash
+
+# make italics work in iTerm and tmux
+# https://medium.com/@dubistkomisch/how-to-actually-get-italics-and-true-colour-to-work-in-iterm-tmux-vim-9ebe55ebc2be
+tic -x $HERE/xterm-256color-italic.terminfo
+tic -x $HERE/tmux-256color.terminfo
 
 # # Dropbox sym links
 
