255008744860f99277bffef28fabd453444ecd1b
Sean Henderson <mail@sean.sh>
2018-07-26 23:22:58 -0700

    Restructure setup scripts.
    .macos/ is now .mkdev, since it will eventually work for other
    platforms.
    
    Removed .bash from scripts for transtion to mixed node/bash system,

--- .macos/mysql.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# configure mysql
-echo "configuring mysql..."
-unset TMPDIR
-mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp
-mysql.server start
-mysql_secure_installation
