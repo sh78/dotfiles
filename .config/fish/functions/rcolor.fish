428f9a8b9b4ab183a374ffc95278c8a83ab55f71
Sean Henderson <mail@sean.sh>
2018-08-05 16:28:50 -0700

    Add optional light/dark arg to random color function.
    +shell +base16 +fish

--- .config/fish/functions/rcolor.fish
+++ .config/fish/functions/rcolor.fish
@@ -1,5 +1,9 @@
-function rcolor --description "Pick a random base16 colorscheme"
-  set color (ls ~/.config/base16-shell/scripts/ | sort -R | tail -1 | string replace '.sh' '')
+function rcolor --description "Pick a random base16 colorscheme ['dark'|'light']"
+  if count $argv > 1
+    set color (ls ~/.config/base16-shell/scripts/ | grep "$argv[1]" | sort -R | tail -1 | string replace '.sh' '')
+  else
+    set color (ls ~/.config/base16-shell/scripts/ | sort -R | tail -1 | string replace '.sh' '')
+  end
   echo $color
   eval $color
 end
