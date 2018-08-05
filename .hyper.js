6ca08c74763de89b7077cc7c65ce2610a2be8cd4
Sean Henderson <mail@sean.sh>
2018-08-05 11:40:26 -0700

    Remove jshint comment from hyper config.
    +terminal +hyper

--- .hyper.js
+++ .hyper.js
@@ -1,5 +1,3 @@
-/*jshint esversion: 6 */
-
 // Future versions of Hyper may add additional config options,
 // which will not automatically be merged into this file.
 // See https://hyper.is#cfg for all currently supported options.
