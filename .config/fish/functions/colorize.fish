d663b72191782de378652274a0a9e3b77cde2251
Sean Henderson <mail@sean.sh>
2018-08-05 16:36:31 -0700

    Let go of solarized, kind of.
    Pick a random dark/light theme instead.
    
    Can still pass an arg to grep for a specific theme name.
    
    +shell +base16 +fish

--- .config/fish/functions/colorize.fish
+++ .config/fish/functions/colorize.fish
@@ -1,7 +1,12 @@
-function colorize --description "Set light/dark theme based on \$COLOR"
-  if [ $COLOR = 'light' ]
-    base16-solarized-light
+function colorize --description "Grep for a base16 theme or set random based on \$COLOR"
+  if count $argv > 1
+    set color (ls ~/.config/base16-shell/scripts/ | grep "$argv[1]" | sort -R | tail -1 | string replace '.sh' '')
+  else if [ $COLOR = 'light' ]
+    set color (ls ~/.config/base16-shell/scripts/ | grep "light" | sort -R | tail -1 | string replace '.sh' '')
   else
-    base16-solarized-dark
+    set color (ls ~/.config/base16-shell/scripts/ | grep "dark" | sort -R | tail -1 | string replace '.sh' '')
   end
+  echo "Theme set to: $color"
+  echo
+  eval $color
 end
--- .config/fish/functions/rcolor.fish
+++
@@ -1,9 +0,0 @@
-function rcolor --description "Pick a random base16 colorscheme ['dark'|'light']"
-  if count $argv > 1
-    set color (ls ~/.config/base16-shell/scripts/ | grep "$argv[1]" | sort -R | tail -1 | string replace '.sh' '')
-  else
-    set color (ls ~/.config/base16-shell/scripts/ | sort -R | tail -1 | string replace '.sh' '')
-  end
-  echo $color
-  eval $color
-end
