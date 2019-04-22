05a84cb4fb86358cc265d14e317607a1ed7bceaa
Sean Henderson <mail@sean.sh>
2019-04-22 13:21:49 -0700

    Explain script supports macOS & Linux
    +shell +fish

--- .config/fish/functions/explain.fish
+++ .config/fish/functions/explain.fish
@@ -1,4 +1,12 @@
+# TODO: this should be a bash script in .bin
 function explain --description "Lookup command on explainshell.com"
     set -l URL_ENCODE (python -c 'import urllib, sys; print urllib.quote_plus(sys.argv[1])' "$argv")
-    open "http://explainshell.com/explain?cmd=$URL_ENCODE"
+
+    if [ "$OSTYPE" = "Linux" ]
+        set open_command xdg-open
+    else
+        set open_command open
+    end
+
+    eval $open_command "http://explainshell.com/explain\?cmd=$URL_ENCODE"
 end
